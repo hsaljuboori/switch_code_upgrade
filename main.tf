8f0afae0874dbc0c1d24a403292977209a8f5a2e
Humam Aljuboori <hsaljubooir.ew@gmail.com>
2024-01-15 18:42:56 -0500

    added main.tf, ansible.cfg

--- main.tf
+++ main.tf
@@ -1,9 +1,22 @@
-
 provider "null" {}
 
 variable "switch_ips" {
   type    = list(string)
   default = ["192.168.86.28", "192.168.86.135"]
+}
+
+resource "null_resource" "preupgrade_check" {
+  count = length(var.switch_ips)
+
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      ./preupgrade_check_enhanced1.sh
+    EOT
+  }
 }
 
 resource "null_resource" "upgrade_switch" {
